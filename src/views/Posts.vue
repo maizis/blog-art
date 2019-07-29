4768d578677055c863b979c17a4018dec6d98745
AdagioSarah <adagiosarah@MacBook-Pro-de-AdagioSarah.local>
2019-07-29 18:00:23 +0200

    Commit 1 - PostList

---
+++ src/views/Posts.vue
@@ -0,0 +1,44 @@
+<template>
+<div class="app">
+  <h1>{{msg}}</h1>
+
+  <table>
+        <tr>
+          <th>Id</th>
+
+        </tr>
+        <tr v-for="(index, array) in arrays" :key="array">
+          <td> {{array.id}} </td>
+
+        </tr>
+    </table>
+
+
+
+
+</div>
+
+
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  data() {
+    return {
+      msg: 'Liste Posts',
+      arrays: [ ],
+    }
+  },
+mounted() {
+    axios
+    .get('https://jsonplaceholder.typicode.com/posts')
+    .then(response => (this.arrays = response))
+    .then(console.log(this.arrays))
+  },
+
+}
+</script>
+
+
